97838d32fe8628434e90fe3806fa135a18c2ba72
Julius Alvarado <nbr1ninrsan2@gmail.com>
2016-03-05 11:09:48 -0800

    initial  commit
    hello world

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var jasmine = require('gulp-jasmine-phantom');
+
+browserSync.init({ server: './' });
+browserSync.stream();
+
+gulp.task('default', function() {
+	gulp.watch('sass/**/*.scss', ['styles']);
+
+});
+
+gulp.task('styles', function() {
+	gulp.src('sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(gulp.dest('./css'));
+});
+
